36ab7a54eb112a86b4eeaf6f90a00a15d67b4b12
r0mj4k <126695989+r0mj4k@users.noreply.github.com>
2023-03-29 14:59:42 +0000

    suma wierszy i kolumn

--- Main.java
+++ Main.java
@@ -5,16 +5,36 @@ class Main {
     int[][] arr= new int[10][10];
     
     Random random = new Random();
+    
+    int[] arrw = new int[10];
+    int[] arrk = new int[10]; 
+    
     for(int i=0;i<10;i++){
-      for(int j=0;j<10;j++)
+      for(int j=0;j<10;j++){
         arr[i][j] = random.nextInt(100) - 49;
+        arrw[i]+=arr[i][j];
+      }
     }
 
+
+
         for(int i=0;i<10;i++){
-      for(int j=0;j<10;j++)
+      for(int j=0;j<10;j++){
         System.out.print(arr[i][j] + "\t");
+        arrk[i]+=arr[j][i];
+      }
           System.out.println();
     }
+
     
+
+
+    for(int i =0;i<10;i++)
+      System.out.println("Suma " + i + " wiersza = " + arrw[i]);
+
+    System.out.println();
+    
+      for(int i =0;i<10;i++)
+      System.out.println("Suma " + i + " kolumny = " + arrk[i]);
   }
 }
